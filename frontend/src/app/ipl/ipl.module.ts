4c8f4aa3a2ffbd4b221369d9b0f3031138681838
username <wecp-dummy@gmail.com>
2025-02-05 04:47:23 +0000

    wecp initial commit

--- frontend/src/app/ipl/ipl.module.ts
+++ frontend/src/app/ipl/ipl.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { IplRoutingModule } from "./ipl-routing.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
+import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+import { Match } from "./types/Match";
+import { RouterModule } from "@angular/router";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+import { VoteComponent } from "./components/vote/vote.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TeamCreateComponent,
+    CricketerCreateComponent,
+    MatchCreateComponent,
+    TicketBookingComponent,
+    VoteComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
+    IplRoutingModule,
+    RouterModule
   ],
   exports: [
     
